ec0345fe54f2976de7e3b50e837f49d1924f0402
Anthony Maringo <alm4cu@virginia.edu>
2020-08-20 15:34:24 -0400

    Initial Commit

---
+++ moves/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moves
+//
+//  Created by Anthony MARINGO on 8/20/20.
+//  Copyright © 2020 Anthony Maringo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
